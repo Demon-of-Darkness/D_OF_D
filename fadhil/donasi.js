53347a2e3dd249656dccb5b2bbb3ea50c29b526d
Demon-of-Darkness <78042290+Demon-of-Darkness@users.noreply.github.com>
2021-02-17 14:21:41 -0300

    Add files via upload

---
+++ fadhil/donasi.js
@@ -0,0 +1,20 @@
+const donasi = (Ig, name) => { 
+	return `       
+┏━━━━━━━━━━━━━━━━━━━━
+┃          DONO
+┣━━━━━━━━━━━━━━━━━━━━
+┣━⊱ *DONO* ❉⊰━━✿
+┃  
+┣━⊱ *INSTA*
+┣⊱ @quardianxd
+┣━⊱ *WHATS*
+┣⊱ http://wa.me/+5553984297333
+┃
+┣━━━━━━━━━━━━━━━━━━━━
+┃  *BOT BY ✞⃟❐͜͡BUMBLEEBE✞⃟♔
+┗━━━━━━━━━━━━━━━━━━━━
+
+`
+}
+
+exports.donasi = donasi
